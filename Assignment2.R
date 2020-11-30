045f99f408001002237f98a02bada9bbc5ec37f0
fholstege <31654652+fholstege@users.noreply.github.com>
2020-11-30 13:24:14 +0100

    Updated Q1

--- Assignment2.R
+++ Assignment2.R
@@ -4,7 +4,6 @@
 ############################
 
 
-
 # Packes required for subsequent analysis. P_load ensures these will be installed and loaded. 
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(plm, 
@@ -25,23 +24,38 @@ set.seed(1234567)
 
 
 ########
-# Question 1A, 1B
+# Question 1A
 ########
 
 # Generate two independent variables, normally distributed
-mX1 <- rnorm(100,mean = 0, sd = 1)
-mX2 <- rnorm(100,mean = 1, sd = 2)
+mX1 <- rnorm(100,mean = 1, sd = 2)
+mX2 <- rnorm(100,mean = 0, sd = 1)
 
 # Parameters 
 a = 0.9
 b1 = 0.5
 b2 = -0.2
 
+# Parameters for sigma2 
+a0 = 2
+b0 = 2
+c1 = 0.01
+c2 = 0.04
+sigma2 = 0.25
 
-# Dependent variable generated 
-y = a + (b1 * mX1) + (b2 * mX2)
+vZ = rgamma(100, scale = a0, shape = a0)
 
-# dataset with simulated data
+########
+# Question 1B
+########
+
+# work with true parameters to get errors
+sigma2_i = 0.25
+
+# generate 100 errors
+errors = rnorm(100,0,sd = sigma2)
+
+# dataset where simulated data will be stored
 dfSimulated_data <- data.frame(y = numeric(), X1 = numeric(), X2 = numeric())
 
 # generate 100 samples of 100, according to the model
@@ -54,13 +68,22 @@ for(i in 1:100){
   # Dependent variable generated 
   y = a + (b1 * mX1) + (b2 * mX2)
   
+  # create errors
+  sigma2 = 0.25
+  errors = rnorm(100,0,sd = sigma2)
+  
+  # add errors to y
+  y <- y + errors
+
   # save for this generated sample
   dfSample = data.frame(y = y, X1 = mX1, X2 = mX2)
   
-  # add to overal dataset
+  # add to overall dataset
   dfSimulated_data = rbind(dfSimulated_data, dfSample)
   
 }
+
+
 
 # test the standard OLS on the simualted data
 lmSimulated <- lm(y ~ X1 + X2, data = dfSimulated_data)
@@ -69,66 +92,70 @@ lmSimulated <- lm(y ~ X1 + X2, data = dfSimulated_data)
 coeftest(lmSimulated, vcov = vcovHC(lmSimulated, type="const"))
 
 # compute white SE
-coeftest(lmSimulated, vcov = vcovHC(lmSimulated, type="HC3"))
+coeftest(lmSimulated, vcov = vcovHC(lmSimulated, type="HC1"))
 
-
-# White standard errors are slightly smaller, but probably too small of a difference to infer anything. 
-# The residuals are homoskedastic (see histogram below, and also logically) so smaller SE not caused by heteroskedasticity
-hist(lmSimulated$residuals, xlim = c(-1e-13, 1e-13), main = "Residuals with OLS", xlab="Residuals")
-
+# White standard errors are slightly bigger, but probably too small of a difference to infer anything. 
+# The residuals are normal (see histogram below, and also logically) so smaller SE not caused by heteroskedasticity
+hist(lmSimulated$residuals, main = "Residuals with OLS", xlab="Residuals")
 
 
 ########
 # Question 1C
 ########
 
-# Parameters for WLS and FLS
-a0 = 2
-b0 = 2
-c1 = 0.01
-c2 = 0.04
-s2 = 0.25
+## For the WLS, we use the est. for sigma2_i, given s2 = 0.25, c1 = 0.01, c2 = 0.04
+# step 1: define z variable with gamma distribution
+vZ = rgamma(10000, scale = a0, shape = b0)
 
-# weights for WLS (given param)
-vZ = rgamma(nrow(dfSimulated_data), scale = a0, shape = a0)
-vWeights = s2 * exp((c1 * vZ^2) +(c2 * vZ))
+# step 2: use definition given to get est. sigma2_i
+sigma2_i_WLS = sigma2 * exp((c1 * vZ^2) +(c2 * vZ))
 
-# weights for FWLS (est. param)
+# step 3: apply these weights in WLS
+lmSimulated_WLS <- lm(y ~ X1+ X2, data = dfSimulated_data, weights=sigma2_i_WLS)
+
+
+## For the WLS, we use the residuals of the OLS to estimate s2, c1, and c2. 
 
 # get residuals from OLS estimate
 residualsOLS <- residuals(lmSimulated)
 
-# regress these with Z = (X1, X2)
-VarEst <- lm(residualsOLS^2 ~ X1 + X2 ,  data = dfSimulated_data)
-summary(VarEst)$coefficients
-mean(VarEst$fitted.values)
-
-# WLS
-lmSimulated_WLS <- lm(y ~ X1+ X2, data = dfSimulated_data, weights=vWeights)
+# model the variables in a dataframe( intercept = sigma2)
+model_sigma2_i <- data.frame( vZ^2, vZ)
 
 
-# FWLS 
-lmSimualted_FWLS <- lm(y ~ X1 + X2, data = dfSimulated_data, weights=VarEst$fitted.values)
+# Take the log of the residuals squared, and see how well these 
+VarEst <- lm(log(residualsOLS^2) ~ vZ + vZ.2 ,data = model_sigma2_i )
 
-# similar SE 
+# take exponent to scale back
+exp(VarEst$coefficients)
+exp(VarEst$coefficients)[1]
+
+# est. parameters 
+sigma2_est = VarEst$coefficients[1]
+c1_est = VarEst$coefficients[2]
+c2_est = VarEst$coefficients[3]
+
+# est. sigma2_i (in version where they are logged)
+sigma2_i_FWLS <- sigma2_est + ((c1_est * vZ^2) + (c2_est * vZ))
+
+# FWLS - uses est. sigma2_ i in weights
+lmSimulated_FWLS <- lm(y ~ X1 + X2, data = dfSimulated_data, weights =exp(sigma2_i_FWLS))
+
+# similar SE - slightly bigger for the FWLS
 summary(lmSimulated_WLS)$coefficient
-summary(lmSimualted_FWLS)$coefficient
+summary(lmSimulated_FWLS)$coefficient
 
+# both have normally distributed errors
+hist(lmSimulated_WLS$residuals, main = "Residuals with WLS", xlab = "Residuals")
+hist(lmSimulated_FWLS$residuals, main = "Residuals with FWLS", xlab = "Residuals")
 
-hist(lmSimulated_WLS$residuals, xlim = c(-1e-13, 1e-13), main = "Residuals with WLS", xlab = "Residuals")
-hist(lmSimualted_FWLS$residuals, xlim = c(-1e-13, 1e-13), main = "Residuals with FWLS", xlab = "Residuals")
-
-nSample = 100
-nSimulations = 100
-
-# weights for WLS (given param)
-vZ = rgamma(nSample, scale = a0, shape = a0)
-vWeights = s2 * exp((c1 * vZ^2) +(c2 * vZ))
-
+########
+# Question 1C
+########
 
 
 # function to simulate results and compare OLS, WLS, FLS 
-simulationResults<- function(nSimulations, nSample, vWeights){
+simulationResults<- function(nSimulations, nSample, a, b1,b2,c1,c2, a0,b0){
   
   # dataframe with results 
   results <- data.frame(Estimator = character(), 
@@ -151,21 +178,62 @@ simulationResults<- function(nSimulations, nSample, vWeights){
     # Dependent variable generated 
     vY = a + (b1 * mX1) + (b2 * mX2)
     
+    # create errors
+    sigma2 = 0.25
+    errors = rnorm(100,0,sd = sigma2)
+    
+    # add to y
+    vY <- vY + errors
+    
     # OLS 
     lmOLS <- lm(vY ~ mX1 + mX2)
     SE_OLS <- summary(lmOLS)$coefficients[,2]
     
-    # WLS
-    lmWLS <- lm(vY ~ mX1 + mX2, weights = vWeights)
+    # for WLS: repeat previous steps
+    
+    # step 1: calc vZ
+    vZ = rgamma(nSample, scale = a0, shape = b0)
+    
+    # step 2: use definition given to get est. sigma2_i
+    sigma2_i_WLS = sigma2 * exp((c1 * vZ^2) +(c2 * vZ))
+    
+    # step 3: apply sigma2_i as weights
+    lmWLS <- lm(vY ~ mX1 + mX2, weights=sigma2_i_WLS)
     SE_WLS <- summary(lmWLS)$coefficients[,2]
     
-    # weights for FWLS (est. param)
-    residualsOLS <- residuals(lmOLS)
-    VarEst <- lm(lmOLS$residuals^2 ~ mX1 + mX2)$fittes.values
+    # for FWLS: again, repeat previous steps
     
-    # FWLS 
-    lmFWLS <- lm(vY ~ mX1 + mX2, data = dfSimulated_data, weights=VarEst)
+    # model the variables in a dataframe( intercept = sigma2)
+    model_sigma2_i <- data.frame( vZ^2, vZ)
+    
+    # get residuals from OLS model
+    residualsOLS <- residuals(lmOLS)
+    
+    # Take the log of the residuals squared, and see how well these 
+    VarEst <- lm(log(residualsOLS^2) ~ vZ + vZ.2 ,data = model_sigma2_i )
+    
+    # est. parameters 
+    sigma2_est = VarEst$coefficients[1]
+    c1_est = VarEst$coefficients[2]
+    c2_est = VarEst$coefficients[3]
+    
+    # est. sigma2_i (in version where they are logged)
+    sigma2_i_FWLS <- sigma2_est + ((c1_est * vZ^2) + (c2_est * vZ))
+    
+    # FWLS - uses est. sigma2_ i in weights
+    lmFWLS <- lm(vY ~ mX1 + mX2, weights =exp(sigma2_i_FWLS))
     SE_FWLS <- summary(lmFWLS)$coefficients[,2]
+    
+    print(iIndexResults)
+    print(SE_FWLS)
+    
+    if(length(SE_FWLS <3)){
+      
+      nMissing <- 3 - length(SE_FWLS)
+      SE_FWLS <- c(SE_FWLS, rep(NA, nMissing))
+      
+    }
+    
     
     # add to the df
     results[iIndexResults,-1] <- c(lmOLS$coefficients, SE_OLS)
@@ -187,8 +255,18 @@ simulationResults<- function(nSimulations, nSample, vWeights){
   
 }
 
+###
+# Question 1D
+#
+###
 
-dfResultSim <- simulationResults(nSimulations, nSample, vWeights)
+# 100 simulations of a sample of 100
+nSample = 100
+nSimulations = 100
+
+
+# simulate results
+dfResultSim <- simulationResults(nSimulations, nSample, a, b1,b2,c1,c2,a0,b0)
 
 # results: FWLS and OLS consistent, WLS not because of the gamma distribution is random each time (but only slight divergencese)
 resultsMelted <- melt(dfResultSim, id.vars = "Estimator")  
@@ -199,7 +277,7 @@ resultsSE <- resultsMelted %>% filter(variable %in% c("a_se", "b1_se", "b2_se"))
 # summarize for mean in plots
 resultsSE_summarized <- resultsSE %>%
   group_by(Estimator, variable) %>%
-  summarize(avg = mean(value))
+  summarise(avg = mean(value))
 
 # create lots to compare the standard errors
 ggplot(resultsSE, aes(x = value, fill = Estimator))+
@@ -216,17 +294,29 @@ resultsParam <- resultsMelted %>% filter(variable %in% c("a", "b1", "b2"))
 # check in dataframe
 resultsParam_summarized <- resultsParam %>%
   group_by(Estimator, variable) %>%
-  summarize(avg = mean(value), sd = sd(value))
+  summarise(avg = mean(value), sd = sd(value))
+resultsParam_summarized
 
+# create lots to compare the coefficients
+ggplot(resultsParam, aes(x = value, fill = Estimator))+
+  geom_histogram(alpha = 0.5, bins = 10, position="identity")+
+  geom_vline( data = resultsParam_summarized, mapping = aes(xintercept = avg))+
+  facet_wrap(~Estimator + variable, scale = "free_x")+
+  scale_x_continuous(breaks =  pretty_breaks())+
+  theme_bw()+
+  labs(y = "Count", x = "Value")
+
+
+###
+# Question 1E
+#
+###
 
 ### under alternative parameters
 c1_0 = 0
 c2_0 = 0
 
-vWeights_0 = s2 * exp((c1_0 * vZ^2) +(c2_0 * vZ))
-
-
-dfResultSim_0 <- simulationResults(nSimulations, nSample, vWeights_0)
+dfResultSim_0 <- simulationResults(nSimulations, nSample, a, b1,b2,c1_0,c2_0,a0,b0)
 resultsMelted_0 <-  melt(dfResultSim_0, id.vars = "Estimator")
 
 # gather results for SE
@@ -235,12 +325,30 @@ resultsSE_0 <- resultsMelted_0 %>% filter(variable %in% c("a_se", "b1_se", "b2_s
 # summarize for mean in plots
 resultsSE_summarized_0 <- resultsSE_0 %>%
   group_by(Estimator, variable) %>%
-  summarize(avg = mean(value))
+  summarise(avg = mean(value))
 
 # create lots to compare the standard errors
 ggplot(resultsSE_0, aes(x = value, fill = Estimator))+
-  geom_histogram(alpha = 0.5, bins = 8, position="identity")+
-  geom_vline( data = resultsSE_summarized, mapping = aes(xintercept = avg))+
+  geom_histogram(alpha = 0.5, bins = 10, position="identity")+
+  geom_vline( data = resultsSE_summarized_0, mapping = aes(xintercept = avg))+
+  facet_wrap(~Estimator + variable, scale = "free_x")+
+  scale_x_continuous(breaks =  pretty_breaks())+
+  theme_bw()+
+  labs(y = "Count", x = "Value")
+
+# gather results for coefficients
+resultsParam_0 <- resultsMelted_0 %>% filter(variable %in% c("a", "b1", "b2"))
+
+# check in dataframe
+resultsParam_summarized_0 <- resultsParam_0 %>%
+  group_by(Estimator, variable) %>%
+  summarise(avg = mean(value), sd = sd(value))
+resultsParam_summarized
+
+# create lots to compare the coefficients
+ggplot(resultsParam_0, aes(x = value, fill = Estimator))+
+  geom_histogram(alpha = 0.5, bins = 10, position="identity")+
+  geom_vline( data = resultsParam_summarized_0, mapping = aes(xintercept = avg))+
   facet_wrap(~Estimator + variable, scale = "free_x")+
   scale_x_continuous(breaks =  pretty_breaks())+
   theme_bw()+
@@ -285,6 +393,7 @@ stargazer(lm_Labincome_size)
 ###################
 # Question 4B, C
 ###################
+dfFamily <- read.csv("DataAS21.csv")
 
 # Correlations to check if instruments are related to the other independent variables
 # Twin variable seems only mildly (0.1) correlated with family size, age child is slightly better (0.3). Same sex and education seem both unrelated to family size(0)
@@ -321,24 +430,25 @@ summary(lm_Famincome_twinIV, diagnostics=TRUE)
 lm_worked_twinIV <- ivreg(weeksworked ~ familysize | twin, data = dfFamily)
 summary(lm_worked_twinIV, diagnostics=TRUE)
 
-
 lm_hours_twinIV <- ivreg(hoursperweek ~ familysize | twin, data = dfFamily)
 summary(lm_worked_twinIV, diagnostics=TRUE)
 
 lm_Labincome_twinIV <- ivreg(laborincome ~ familysize | twin, data = dfFamily)
 summary(lm_worked_twinIV, diagnostics=TRUE)
 
+colnames(dfFamily)
+
 # re-restimate models with the age child instrument variable (explained part)
-lm_Famincome_ageChildIV <- ivreg(familyincome ~ familysize | ageChild, data = dfFamily)
+lm_Famincome_ageChildIV <- ivreg(familyincome ~ familysize | agechild, data = dfFamily)
 summary(lm_Famincome_ageChildIV, diagnostics=TRUE)
 
-lm_worked_ageChildIV <- ivreg(weeksworked ~ familysize | ageChild, data = dfFamily)
+lm_worked_ageChildIV <- ivreg(weeksworked ~ familysize | agechild, data = dfFamily)
 summary(lm_worked_ageChildIV, diagnostics=TRUE)
 
-lm_hours_ageChildIV <- ivreg(hoursperweek ~ familysize | ageChild, data = dfFamily)
+lm_hours_ageChildIV <- ivreg(hoursperweek ~ familysize | agechild, data = dfFamily)
 summary(lm_worked_ageChildIV, diagnostics=TRUE)
 
-lm_Labincome_ageChildIV <- ivreg(laborincome ~ familysize | ageChild, data = dfFamily)
+lm_Labincome_ageChildIV <- ivreg(laborincome ~ familysize | agechild, data = dfFamily)
 summary(lm_worked_ageChildIV, diagnostics=TRUE)
 
 
